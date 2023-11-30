3c1a51ed96915eb90dba1dd2e69bda6a1b8a946a
axdjuraev <axdjuraev@gmail.com>
2023-11-30 18:16:02 +0500

    feat (BaseApi): _safe_response

--- pycronorg/sync/base.py
+++ pycronorg/sync/base.py
@@ -2,6 +2,8 @@ import requests
 from abc import ABC
 from typing import Optional
 
+from pycronorg.exceptions import ResponseExecption
+
 
 class BaseApi(ABC):
     _AUTH_KEY = 'Authorization'
@@ -27,4 +29,10 @@ class BaseApi(ABC):
         self._base_path = base_path or self._DEFAULT_BASE_PATH
         self._url = f"{self._base_host}/{self._base_path}"
         self._proxy_request = proxy_request or requests
+
+    def _safe_response(self, res: requests.Response):
+        if not res.ok:
+            raise ResponseExecption(f"[{res.status_code}][`{res.url}`]: `{res.text}`")
+
+        return res
 
